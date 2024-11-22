7ab4149670b7c220e281edaf460ba81564bfbf68
AmirKafi <doostkafiamir@gmail.com>
2024-11-22 17:01:13 +0330

    Clean solution

--- PageExtractorByContour.py
+++ PageExtractorByContour.py
@@ -5,22 +5,21 @@ from processors.ContourCornersDetector import ContourCornersDetector
 
 contour_page_extractor = ContourCornersDetector()
 
-img = cv2.imread('assets/QalamChi.jpg')
+file_name = 'QalamChi.jpg'
+img = cv2.imread('assets/'+file_name)
 final = contour_page_extractor(img)
 cv2.imshow('original', img)
 cv2.imshow('final', final)
+cv2.imwrite(f"output/{file_name}", final)
 cv2.waitKey(0)
 
-img = cv2.imread('assets/photo_2024-11-20_17-16-00.jpg')
+file_name = "Cropped2.jpg"
+img = cv2.imread(f'assets/{file_name}')
 final = contour_page_extractor(img)
 cv2.imshow('original', img)
 cv2.imshow('final', final)
+cv2.imwrite(f"output/{file_name}", final)
 cv2.waitKey(0)
-
-img = cv2.imread('assets/photo_2024-11-20_12-47-14.jpg')
-final = contour_page_extractor(img)
-cv2.imshow('original', img)
-cv2.imshow('final', final)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
